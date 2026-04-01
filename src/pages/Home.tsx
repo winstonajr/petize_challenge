6fd5e8366d324faa719feafe2e718dc3de2f2e1a
Winston <winston.almeidamjr@gmail.com>
2026-04-01 00:08:57 -0300

    feat: create home page with full-screen centering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,35 @@
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import SearchForm from '../features/search/components/SearchForm';
+
 export default function Home() {
-  return <div>Enter</div>;
+  return (
+    <Flex
+      height={{ base: '100dvh', md: '100vh' }}
+      width='100vw'
+      alignItems='center'
+      justifyContent='center'
+      flexDirection='column'
+      bg='var(--bg-white)'
+      overflow='hidden'
+      position='fixed'
+      top={0}
+      left={0}
+    >
+      <Heading
+        fontSize={{ base: '3.125rem', md: '5rem' }}
+        fontWeight='medium'
+        textAlign='center'
+        mb={{ base: '2rem', md: '3.25rem' }}
+      >
+        <Text as='span' color='var(--brand-blue)'>
+          Search
+        </Text>{' '}
+        <Text as='span' color='var(--brand-purple)'>
+          d_evs
+        </Text>
+      </Heading>
+
+      <SearchForm />
+    </Flex>
+  );
 }
