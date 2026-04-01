ca1ec195845cdedf52fc6fdf4ed8f20b5565784d
Winston <winston.almeidamjr@gmail.com>
2026-04-01 10:27:17 -0300

    feat: setup routing and page integrations

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
-import App from './App.tsx';
 import i18n from './lib/i18n/config';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <ChakraProvider>
+      <I18nextProvider i18n={i18n}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </I18nextProvider>
+    </ChakraProvider>
   </StrictMode>,
 );
